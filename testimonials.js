ea23f47b833ba44f1c39593eabe2fb15416ecedf
buckrebeka <103574108+buckrebeka@users.noreply.github.com>
2024-01-26 18:55:49 +0000

    Add files via upload

---
+++ testimonials.js
@@ -0,0 +1,27 @@
+const testimonials =[
+    {
+        quote:"Our company,we do ongoing research with our target audience.This includes 30-45 minutes phone interviews.It was difficult to conduct the interview,really listen,and ask good follow up questions",
+        avatar:"avatar1",
+        name:"Diana Ayi",
+        title:"Musician",
+    },
+    {
+        quote:"Our company,we do ongoing research with our target audience.This includes 30-45 minutes phone interviews.It was difficult to conduct the interview,really listen,and ask good follow up questions",
+        avatar:"avatar2",
+        name:"Fo Doh Laba",
+        title:"Footerballer",
+    },
+    {
+        quote:"Our company,we do ongoing research with our target audience.This includes 30-45 minutes phone interviews.It was difficult to conduct the interview,really listen,and ask good follow up questions",
+        avatar:"avatar3",
+        name:"Rebeka Buckner",
+        title:"Web Developer",
+    },
+
+    {
+        quote:"Our company,we do ongoing research with our target audience.This includes 30-45 minutes phone interviews.It was difficult to conduct the interview,really listen,and ask good follow up questions",
+        avatar:"avatar4",
+        name:"Gerry Tama",
+        title:"Politician",
+    },
+]
